2dd97f7493fe1964fef27ee5450fbf4d282b9531
Martin Peth <martin@peth.pl>
2025-01-20 00:00:08 +0100

    update: gitignore

---
+++ include/config/ConfigDefines.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_DEFINES_H
+#define CONFIG_DEFINES_H
+
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
+#define WIFI_CHECK_INTERVAL 500
+
+#define MQTT_BROKER ""
+#define MQTT_PORT 1883
+#define MQTT_USER ""
+#define MQTT_PASSWORD ""
+#define MQTT_RETRY_INTERVAL 5000
+
+#endif
