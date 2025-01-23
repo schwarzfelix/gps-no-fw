d3880a811f1e60db2ebd88054cf48504d768812a
Martin Peth <martin@peth.pl>
2025-01-23 16:04:34 +0100

    add: wifi-manager

--- include/WiFiManager.h
+++ include/WiFiManager.h
@@ -5,4 +5,57 @@
 #include "ConfigManager.h"
 #include "Logger.h"
 
+enum class WifiStatus {
+    DISCONNECTED,
+    CONNECTING,
+    CONNECTED, 
+    CONNECTION_FAILED,
+    WRONG_PASSWORD,
+    NO_SSID_AVAILABLE,
+    __DELIMITER__
+};
+
+class WifiManager {
+private:
+    WifiManager()
+        : status(WifiStatus::DISCONNECTED)
+        , lastAttempt(0)
+        , connectionAttempts(0)
+        , configManager(ConfigManager::getInstance())
+        , log(Logger::getInstance()) {}
+
+    WifiStatus status;
+    uint32_t lastAttempt;
+    uint8_t connectionAttempts;
+
+    ConfigManager& configManager;
+    Logger& log;
+
+    const char *getWifiStatusString(WifiStatus status);
+    constexpr size_t getWifiStatusCount() {return static_cast<size_t>(WifiStatus::__DELIMITER__);};
+
+public:
+    static WifiManager& getInstance() {
+        static WifiManager instance;
+        return instance;
+    }
+
+    bool begin();
+    void update();
+    bool connect();
+    void disconnect();
+    void reconnect();
+    void printStatus();
+    bool isConnected();
+    void setAutoReconnect(bool isEnabled);
+    
+    String getIP();
+    String getSSID();
+    int32_t getRSSI();
+    uint8_t getConnectionAttempts();
+
+    WifiStatus getStatus();
+    const char* getStatusString() { return getWifiStatusString(status); };
+};
+
 #endif
---
+++ src/WifiManager.cpp
@@ -0,0 +1,124 @@
+#include "WifiManager.h"
+
+
+const char* WifiManager::getWifiStatusString(WifiStatus status) {
+    switch (status) {
+        case WifiStatus::DISCONNECTED: return "DISCONNECTED";
+        case WifiStatus::CONNECTING: return "CONNECTING";
+        case WifiStatus::CONNECTED: return "CONNECTED";
+        case WifiStatus::CONNECTION_FAILED: return "CONNECTION_FAILED";
+        case WifiStatus::WRONG_PASSWORD: return "WRONG_PASSWORD";
+        case WifiStatus::NO_SSID_AVAILABLE: return "NO_SSID_AVAILABLE";
+        default: return "UNKNOWN";
+    }
+};
+
+bool WifiManager::begin(){
+    log.debug("WifiManager", "Initializing WifiManager...");
+
+    RuntimeConfig& config = configManager.getRuntimeConfig();
+
+    if(strlen(config.wifi.ssid) == 0) {
+        log.warning("WifiManager", "No SSID available, skipping WifiManager initialization");
+        return false;
+    } else if (strlen(config.wifi.password) == 0) {
+        log.warning("WifiManager", "No password available, skipping WifiManager initialization");
+        return false;
+    } 
+
+    WiFi.mode(WIFI_STA);
+    return true;
+}
+
+bool WifiManager::isConnected() {
+    return status == WifiStatus::CONNECTED && WiFi.status() == WL_CONNECTED;
+}
+
+bool WifiManager::connect() { 
+    if(status == WifiStatus::CONNECTED) {
+        return true;
+    }
+
+    RuntimeConfig& config = configManager.getRuntimeConfig();
+
+    log.debug("WifiManager", "Attempting to connect to Wifi...");
+    Serial.printf("%s (%d)\n", config.wifi.ssid, strlen(config.wifi.ssid));
+    Serial.printf("%s (%d)\n", config.wifi.password, strlen(config.wifi.password));
+    WiFi.setMinSecurity(WIFI_AUTH_WPA_PSK); 
+    WiFi.begin(config.wifi.ssid, config.wifi.password);
+
+    status = WifiStatus::CONNECTING;
+    lastAttempt = millis();
+    connectionAttempts = 1;
+
+    return true;
+}
+
+void WifiManager::disconnect() {
+    log.debug("WifiManager", "Disconnecting from Wifi...");
+
+    WiFi.disconnect();
+    status = WifiStatus::DISCONNECTED;
+}
+
+void WifiManager::update(){
+    RuntimeConfig &config = configManager.getRuntimeConfig();
+    if(status == WifiStatus::CONNECTING){
+        if (WiFi.status() == WL_CONNECTED){
+            status = WifiStatus::CONNECTED;
+            connectionAttempts = 0;
+
+            char msgBuffer[64];
+            snprintf(msgBuffer, sizeof(msgBuffer), "Connected to Wifi-AP with IP: %s", WiFi.localIP().toString().c_str());
+            log.debug("WifiManager", msgBuffer);
+
+            return;
+        }
+
+        if (millis() - lastAttempt >= config.wifi.checkInterval) {
+            lastAttempt = millis();
+            connectionAttempts++;
+
+            Serial.printf("Connection Attempts: %d (%d)\n", connectionAttempts, config.wifi.maxConnectionAttempts);
+            if (connectionAttempts >= config.wifi.maxConnectionAttempts) {
+                status = WifiStatus::CONNECTION_FAILED;
+                char msgBuffer[192];
+                snprintf(msgBuffer, sizeof(msgBuffer), "Failed to connect to Wifi-AP ('%s') due reaching max connection attempts", config.wifi.ssid);
+                log.error("WifiManager", msgBuffer);
+
+                return;
+            }
+        }
+    } else if (status == WifiStatus::CONNECTED && WiFi.status() != WL_CONNECTED) {
+        status = WifiStatus::DISCONNECTED;
+        char msgBuffer[192];
+        snprintf(msgBuffer, sizeof(msgBuffer), "Lost connection to Wifi-AP ('%s')", config.wifi.ssid);
+        log.warning("WifiManager", msgBuffer);
+
+        if (config.wifi.autoReconnect && millis() - lastAttempt >= config.wifi.reconnectInterval) {
+            connect();
+        }
+    }
+}
+
+WifiStatus WifiManager::getStatus(){
+    return status;
+}
+
+String WifiManager::getIP() {
+    IPAddress localIP = WiFi.localIP();
+    return String(localIP[0]) + "." + String(localIP[1]) + "." + String(localIP[2]) + "." + String(localIP[3]);
+}
+
+String WifiManager::getSSID() {
+    return WiFi.SSID();
+}
+
+int32_t WifiManager::getRSSI() {
+    return WiFi.RSSI();
+}
+
+uint8_t WifiManager::getConnectionAttempts() {
+    return connectionAttempts;
+}
+
